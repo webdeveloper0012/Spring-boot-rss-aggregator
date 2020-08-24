86442ae256e66e0931d743effbf36bef47cb7659
Web Developer <60765936+webdeveloper0012@users.noreply.github.com>
2020-08-24 00:54:41 -0300

    Update RssFeedMessageSource.java

--- src/main/java/org/thevlad/rss/RssFeedMessageSource.java
+++ src/main/java/org/thevlad/rss/RssFeedMessageSource.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-
+ 
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.io.input.BOMInputStream;
 import org.apache.http.client.methods.CloseableHttpResponse;
